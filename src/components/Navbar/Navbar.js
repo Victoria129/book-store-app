fa1bdb2e78c8357db3b021464af194e2be1bb96c
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-01-07 03:49:32 +0300

    Add Styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav className="nav">
     <header>
-      <h1>BookStore</h1>
+      <h1>Bookstore CMS</h1>
     </header>
 
     <NavLink to="/">
