c8e7266a6f2de78c16ddb8900d59ba3f9c68d975
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-03-01 13:04:55 +0300

    use redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
-export default configureStore({
+configureStore({
   reducer: {
     books: bookReducer,
     categories: categoryReducer,
   },
 });
+
+export default configureStore;
