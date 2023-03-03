9dd9ddd79f0af5b5cfe4a772cb1ea00ce1b9ac4c
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-03-03 14:37:36 +0300

    add redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -9,6 +9,24 @@ const bookSlice = createSlice({
       author: 'person1',
 
     },
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
   ],
   reducers: {
     addBook: (state, action) => {
